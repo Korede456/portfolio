400fc8b0ed61921951990d2c88371361e1c10dfb
korede456 <koredethompson456@gmail.com>
2024-05-22 21:14:41 +0100

    changed the entire font family

--- my_portfolio/src/main.jsx
+++ my_portfolio/src/main.jsx
@@ -5,8 +5,17 @@ import { Box, ChakraProvider } from "@chakra-ui/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ChakraProvider >
-      <Box w="100%" px={{ sm: "2%", md: "5%", lg: "10%" }} m="0" bg="#181A2A" color="#fff"border="2px solid gold" rounded="10" fontFamily="monospace">
+    <ChakraProvider>
+      <Box
+        w="100%"
+        px={{ sm: "2%", md: "5%", lg: "10%" }}
+        m="0"
+        bg="#181A2A"
+        color="#fff"
+        border="2px solid gold"
+        rounded="10"
+        fontFamily="Verdana"
+      >
         <App />
       </Box>
     </ChakraProvider>
