84d3fe6875532fce77cbd6513d3754bd5514b1f7
korede456 <koredethompson456@gmail.com>
2024-05-22 20:31:18 +0100

    update

--- my_portfolio/src/components/Footer.jsx
+++ my_portfolio/src/components/Footer.jsx
@@ -4,8 +4,8 @@ const Footer = () => {
   return (
     <Box>
       <Heading>Contact Me</Heading>
-      <Flex>
-        <Text>
+      <Flex w={{base:"100%", md:"60%"}} align="center" justify="center" gap="10">
+        <Text w="300">
           <Link href="mailto:koredethompson456@gmail.com">
             koredethompson456@gmail.com
           </Link>
@@ -13,7 +13,7 @@ const Footer = () => {
         <Text>
           <Link href="tel:+2347033300647">+2347033300647</Link>
         </Text>
-        <Text></Text>
+        <Text>Kubwa,  Abuja,  Nigeria</Text>
       </Flex>
     </Box>
   );
